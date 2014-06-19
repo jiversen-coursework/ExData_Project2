76656e8c7fa034a88a90adc32a7391aa32c7e8cf
John R Iversen <john@johniversen.org>
2014-06-18 17:37:26 -0700

    Plot1

--- plot1.R
+++ plot1.R
@@ -1,10 +1,24 @@
 # Project 2 on air pollution
-
-#read data
-NEI<-readRDS('NEI_data/summarySCC_PM25.rds')
-SCC<-readRDS('NEI_data/Source_Classification_Code.rds')
+# Plot1
+# JRI 6/18/14
 
 #Have total emissions from PM2.5 decreased in the United States from 1999 to
 #2008? Using the base plotting system, make a plot showing the total PM2.5
 #emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 
+# Answer: Yes, total emissions have decreased
+
+#read data
+NEI<-readRDS('NEI_data/summarySCC_PM25.rds')
+SCC<-readRDS('NEI_data/Source_Classification_Code.rds')
+
+# boxplot--not useful cause of some enormous outliers
+boxplot(Emissions~year, data=NEI, notch=TRUE)
+
+#find total grouped by year
+years <- as.factor(unique(NEI$year))
+yearlyTotalEmissions <- tapply(NEI$Emissions, NEI$year, sum)
+
+png(file="plot1.png",width=480,height=480)
+barplot(yearlyTotalEmissions,names.arg=years, xlab="Year", ylab="Total Emissions [PM2.5]",main="Total Emissions Decreased from 1999 to 2008")
+dev.off()
