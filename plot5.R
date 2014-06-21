8a9624ad40fb9aa84f38ea951b8e3a2fd6339770
John R Iversen <john@johniversen.org>
2014-06-20 21:24:34 -0700

    plot 5 implementation

--- plot5.R
+++ plot5.R
@@ -1,8 +1,32 @@
 # Project 2 on air pollution
+# Plot5
+# JRI 6/20/14
+
+#How have emissions from motor vehicle sources changed from 1999–2008 in
+#Baltimore City?
+
+#Answer: They have declined considerably
 
 #read data
 NEI<-readRDS('NEI_data/summarySCC_PM25.rds')
 SCC<-readRDS('NEI_data/Source_Classification_Code.rds')
 
-#How have emissions from motor vehicle sources changed from 1999–2008 in
-#Baltimore City?
+#get sources in Baltimore
+baltimoreNEI = NEI[NEI$fips=="24510",]
+
+#ASSUMPTION: motor vehicles is union of onroad and non-road types
+vehicleTypes = c("NON-ROAD","ON-ROAD")
+vehicleNEI = baltimoreNEI[baltimoreNEI$type %in% vehicleTypes,]
+
+#tabulate
+yearlyTotalEmissions <- tapply(vehicleNEI$Emissions, vehicleNEI$year, sum)
+
+#make a bar plot
+png(file="plot5.png",width=480,height=480)
+barplot(yearlyTotalEmissions, xlab="Year", ylab="Total Emissions [PM2.5]",main="Emissions from motor vehicles in Baltimore")
+dev.off()
+
+# -- optional --
+#ggplot2 version: slower, and I find it less attractive
+#library(ggplot2)
+#qplot(as.factor(year),data=vehicleNEI,geom="bar",weight=Emissions)
