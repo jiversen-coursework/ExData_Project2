8a49f0b174b21f5ee994d00e42e6765c21412175
John R Iversen <john@johniversen.org>
2014-06-20 21:24:25 -0700

    plot 4 implementation

--- plot4.R
+++ plot4.R
@@ -1,8 +1,31 @@
 # Project 2 on air pollution
+# Plot4
+# JRI 6/20/14
+
+#Across the United States, how have emissions from coal combustion-related
+#sources changed from 1999–2008?
+
+#Answer: They remained steady through 2005 then decreased in 2008
+
+library(plyr)
 
 #read data
 NEI<-readRDS('NEI_data/summarySCC_PM25.rds')
 SCC<-readRDS('NEI_data/Source_Classification_Code.rds')
 
-#Across the United States, how have emissions from coal combustion-related
-#sources changed from 1999–2008?
+#find entries on SCC that use coal
+# list of last words of EI.Sector
+tmp<-llply(as.character(SCC$EI.Sector),function(x){a<-strsplit(x," - ")[[1]]; a[length(a)]})
+isCoal <- (tmp=="Coal")
+coalSCC <- as.character(SCC$SCC[isCoal]) #vector of the SCC codes for coal plants
+
+#find emissions data for the coal sources
+coalNEI <- NEI[NEI$SCC %in% coalSCC,]
+
+#tabulate
+yearlyTotalEmissions <- tapply(coalNEI$Emissions, coalNEI$year, sum)
+
+#make a bar plot
+png(file="plot4.png",width=480,height=480)
+barplot(yearlyTotalEmissions, xlab="Year", ylab="Total Emissions [PM2.5]",main="Emissions from coal-burning sources")
+dev.off()
